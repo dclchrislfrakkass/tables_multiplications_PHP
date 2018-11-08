ae3c359df9a52f4cc0cf4508629a91fb9200d4dc
gen006 <gen006@gen006pc>
2018-11-08 11:39:55 +0100

    design ok

---
+++ index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta http-equiv="X-UA-Compatible" content="ie=edge">
+<link rel="stylesheet" href="css/style.css">
+<title>Tables de multiplications</title>
+</head>
+<header>
+
+<h1>TABLES DE MULTIPLICATIONS</h1>
+<br>
+
+<form id="multiplication">
+<div id="choice">
+<select name="tables">
+<option value="1">1</option>
+<option value="2">2</option>
+<option value="3">3</option>
+<option value="4">4</option>
+<option value="5">5</option>
+<option value="6">6</option>
+<option value="7">7</option>
+<option value="8">8</option>
+<option value="9">9</option>
+<option value="10">10</option>
+</select>
+<br>
+Choisis la table à afficher
+<br>
+<input type="submit" value="Voir">
+</form>
+
+</header>
+
+
+<body>
+
+
+
+</div>
+<div id="affichage">
+</div>
+
+</body>
+
+
+
+
+
+
+</html>
